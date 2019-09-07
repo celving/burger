668bf75821ce4f53200aecf551f6d2a3cf3578c4
Colin Elving <viewtifuljho@hotmail.com>
2019-09-07 17:47:52 -0500

    config files done

---
+++ config/orm.js
@@ -0,0 +1,55 @@
+var connection = require("./connection.js");
+
+var orm = {
+
+    selectAll = function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne = function (table, col, val, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += col;
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += val;
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    updateOne: function(table, col, val, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += col;
+        queryString += " = ";
+        queryString += val;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+    }
+};
+
+module.exports = orm;
